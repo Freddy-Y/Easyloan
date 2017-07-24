3456fafb100c9e88b0e69a121aedafc196a1e03e
panjunwen <i@panjunwen.com>
2017-07-24 10:37:28 +0800

    Initial commit

---
+++ app/src/main/java/com/ruanko/easyloan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ruanko.easyloan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
